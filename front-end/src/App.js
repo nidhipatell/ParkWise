e27ac7854a2b4ed219337ad58155daca16a9fb81
nandinipatel <patelnandini26@gmail.com>
2020-10-17 17:07:51 -0400

    minor changeS

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 import Home from './Component/Home';
-import Authenticate from './Component/Authenticate';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -12,7 +11,6 @@ function App() {
       <div>
         <Switch>
           <Route path="/" component={Home} />
-          <Route path="/register" component={AuthenticateScreen} />
         </Switch>
       </div>
     </Router>
