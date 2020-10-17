aa2aaa2c10d8c9ae0158217823a126861a48acdb
Niraj Patel <786nirajpatel@gmail.com>
2020-10-17 18:22:27 -0400

    login.css

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import './App.css';
 import Home from './Component/Home';
 
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
