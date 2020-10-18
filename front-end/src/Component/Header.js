10d6318130cd6633239fb4870d18e70cf6a2f028
Niraj Patel <786nirajpatel@gmail.com>
2020-10-18 03:44:18 -0400

    Edits to header.css

--- front-end/src/Component/Header.js
+++ front-end/src/Component/Header.js
@@ -4,11 +4,14 @@ class Header extends Component {
     render() {
         return (
             <div className="Header">
-                <ul className = "menu-ul">
-                    <li><a href="#Contact Us">Contact Us</a></li>
-                    <li><a href="Login.js">Authenticate</a></li>                
-                    <li><a href="#About Us">About Us</a></li>
-                </ul>
+                <nav>
+                    <ul className = "menu-ul">
+                        <li><a href="#About Us" className="NavBtn">Sign Up</a></li>
+                        <li><a href="#About Us" className="NavBtn">Login In</a></li>                
+                        <li><a href="#About Us" className="NavBtn">Contact Us</a></li>
+                        <li><a href="#About Us" className="NavBtn">About Us</a></li>
+                    </ul>
+                </nav>
             </div>
         );
     }
