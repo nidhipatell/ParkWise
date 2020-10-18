138adb99322036ae90706824c83d4d9c9fa9cb5c
nandinipatel <patelnandini26@gmail.com>
2020-10-18 00:47:01 -0400

    background

--- front-end/src/Component/Footer.js
+++ front-end/src/Component/Footer.js
@@ -4,12 +4,12 @@ class Footer extends React.Component {
         return (
             <div>
                 <footer className="footer">
-                    <h3> ParkWise | ParkNGo </h3>
+                    <h3> ParkWise | Park N' Go </h3>
                     <div class="contributors">
-                        <a>Nandini Patel</a>
-                        <a>Nidhi Patel</a>
-                        <a>Niraj Patel</a>
-                        <a>Hardik Hajela</a>
+                        <a>Nandini Patel - </a>
+                        <a>Nidhi Patel - </a>
+                        <a>Niraj Patel - </a>
+                        <a>Hardik Hajela - </a>
                         <a>Het Patel</a>
                     </div>
                     <p>Group 26</p>
