d8c2f4dbc5f33fde787752ea3f0118ea2275763d
nidhipatell <50842384+nidhipatell@users.noreply.github.com>
2020-10-19 12:23:47 -0400

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import Home from './Component/Home';
 import Login from './Component/Login';
+import Signup from './Component/Signup';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
