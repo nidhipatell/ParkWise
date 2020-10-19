3c84d7bd97eceeb442dd11d532e4688d5448a5ea
nidhipatell <50842384+nidhipatell@users.noreply.github.com>
2020-10-19 13:43:17 -0400

    updated

--- front-end/src/Component/Signup.js
+++ front-end/src/Component/Signup.js
@@ -6,8 +6,10 @@ export default class Signup extends Component
 {
     render(){
         return(
-            <div className="background"> 
-            <p> This is the sign up page </p>
+            <div> 
+           <body className="background">
+
+           </body>
             </div>
         );
     }
