ded4e99a6be9957e4f0ab54ce0567551e7b85801
nidhipatell <50842384+nidhipatell@users.noreply.github.com>
2020-10-26 20:31:59 -0400

    Updated Signup Page

--- front-end/src/Component/Signup.js
+++ front-end/src/Component/Signup.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 import Header from './Header.js';
 import Footer from './Footer.js';
 
@@ -8,11 +9,18 @@ export default class Signup extends Component
 {
     render(){
         return(
-            <div>
-                <Header /> 
-                <p>This is the sign up page</p>
-                <Footer />
+            <div className="signup-container">
+                <Header />
+                <div class="signup-box">
+                    <h1>Login</h1>
+                    <div class="textbox">
+                        <i class="fas fa-user"></i>
+                        <input type="text" placeholder="Email"></input>
+                    </div>
 
+                    
+                    </div>
+                <Footer />
             </div>
         );
     }
