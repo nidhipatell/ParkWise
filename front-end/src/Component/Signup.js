161bcdc7c626d9b6c8f858563de52dfe3a8e03b6
nidhipatell <50842384+nidhipatell@users.noreply.github.com>
2020-10-26 20:34:52 -0400

    added more features to sign up

--- front-end/src/Component/Signup.js
+++ front-end/src/Component/Signup.js
@@ -12,12 +12,28 @@ export default class Signup extends Component
             <div className="signup-container">
                 <Header />
                 <div class="signup-box">
-                    <h1>Login</h1>
+                    <h1>Sign up</h1>
                     <div class="textbox">
                         <i class="fas fa-user"></i>
                         <input type="text" placeholder="Email"></input>
                     </div>
 
+                    <div class="text">
+                        <i class="fas fa-lock"></i>
+                        <input type="Name" placeholder="Name"></input>
+                    </div>
+
+                    <div class="text">
+                        <i class="fas fa-lock"></i>
+                        <input type="lastname" placeholder="Last Name"></input>
+                    </div>
+
+                    <div class="textbox">
+                        <i class="fas fa-lock"></i>
+                        <input type="password" placeholder="Password"></input>
+                    </div>
+
+                    <input type="button" class="btn" value="Sign in"></input>
                     
                     </div>
                 <Footer />
