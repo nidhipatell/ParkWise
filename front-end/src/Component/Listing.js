6a27bef19c6b9c2a0e6ddcb761172c3155e8bfcc
Niraj Patel <786nirajpatel@gmail.com>
2020-11-28 05:56:43 -0500

    Data from the firebase is retrived and displayed on the app

--- front-end/src/Component/Listing.js
+++ front-end/src/Component/Listing.js
@@ -11,7 +11,7 @@ export default function Listing(props) {
 
     useEffect(()=>{
         const locationRef = firebase.database().ref("major_locations/"+location);
-        locationRef.once("value",(snapshot)=> {
+        locationRef.on("value",(snapshot)=> {
             console.log(snapshot.val());
             const locations = snapshot.val();
             const locationsList = []
@@ -19,6 +19,8 @@ export default function Listing(props) {
                 console.log(locations[loc]);
                 locationsList.push(locations[loc]);    
             }
+            console.log(locationsList);
+            setlocationList(locationsList);
         });
     },[])
 
@@ -28,7 +30,11 @@ export default function Listing(props) {
                 <Header></Header>
                 <h1 className="display-5 location-parking">Parking in {location}</h1>
                 <hr className="location-hr"></hr>
-                
+                <div className="location-parking">
+                    {locationsList ? locationsList.map((location) => (
+                    <h1>{location}</h1>
+                    )): "Loading"}
+                </div>
             </div>
         )
 }
