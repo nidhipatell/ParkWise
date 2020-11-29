6b7101d333016ce831b7b2ea321fd43e26fca7d2
nandinipatel <patelnandini26@gmail.com>
2020-11-29 18:32:24 -0500

    recent changes

--- front-end/src/Component/Home.js
+++ front-end/src/Component/Home.js
@@ -44,7 +44,7 @@ render(){
                         Book for your parking spot at a nearby driveway at a convenient location
                         </div>
                         <div className="icon col-md-5">
-                            <img src="images/book.png" alt="" />
+                            <img src="images/book1.png" alt="" />
                         </div>
                     </div>
                     <div className="thirdrow row">
@@ -53,21 +53,27 @@ render(){
                         </div>
                         <div className="budgetPara col-md-7">
                             <h2>Budget</h2>
-                                Get a budget friendly parking spot for a longer period of time
+                        Get a budget friendly parking spot for a longer period of time
                         </div>
                     </div>
                 </div>
             </div>
         <div className="cities">
-        <h3 className="city"> Featured Cities</h3>
-        <div className="imageTor">Toronto</div>
-        <div className="imageOta">Ottawa</div>
-        <div className="imageVan">Vancover</div>
-        <div className="imageAlb">Alberta</div>
-        <div className="imageMon">Monteral</div>
-        <div className="imageQue">Quebec</div>
+            <h3 className="cityTitle"> Featured Cities</h3>
+            <div className="allCities container">
+                <div className="row firstrow">
+                    <div className="imageTor col-md-3">Toronto</div>
+                    <div className="imageOta col-md-3">Ottawa</div>
+                    <div className="imageVan col-md-3">Vancover</div>
+                </div>    
+                <div className="row secondrow">
+                <div className="imageAlb col-md-3">Alberta</div>
+                <div className="imageMon col-md-3">Montreal</div>
+                <div className="imageQue col-md-3">Quebec</div>
+                </div>
+            </div>
         </div>
-        
+        <Footer/>
     </div>
     );
     }
