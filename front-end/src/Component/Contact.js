9d37ffe7266cf9263384c3b06321b2eb133278d5
hetpatel14 <hetpatel91314@gmail.com>
2020-11-29 23:36:52 -0500

    Contact us

--- front-end/src/Component/Contact.js
+++ front-end/src/Component/Contact.js
@@ -1,4 +1,4 @@
-import React, {Component, useRef, useState} from 'react';
+import React, { Component, useRef, useState } from 'react';
 import axios from 'axios';
 import Header from './Header.js';
 import Footer from './Footer.js';
@@ -7,14 +7,14 @@ import { AuthProvider, useAuth } from "../contexts/AuthContext"
 import { Link, useHistory } from "react-router-dom"
 
 
-export default function Contact() {
+export default function Login() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const { login } = useAuth()
     const [error, setError] = useState("")
     const [loading, setLoading] = useState(false)
     const history = useHistory()
-    
+
 
     async function handleSubmit(e) {
         e.preventDefault()
@@ -30,31 +30,39 @@ export default function Contact() {
         setLoading(false)
 
     }
-    
+
     return (
         <div className="signup-container">
             <Header></Header>
             <Container className="d-flex align-item-center justify-content-center" style={{ minHeight: "100vh", }}>
-                <div className="w-100" style={{ maxWidth: "400px", alignItems: "center", display: "flex", justifyContent: "center", textAlign: "center", flexDirection: "column"}}>
-                    <Card style={{ background: "none", border: "none"}}>
+                <div className="w-100" style={{ maxWidth: "400px", alignItems: "center", display: "flex", justifyContent: "center", textAlign: "center", flexDirection: "column" }}>
+                    <Card style={{ background: "none", border: "none" }}>
                         <Card.Body>
-                            <h2 className="text-center mb-4">Log In</h2>
+                            <h2 className="text-center mb-4">Contact Us</h2>
                             {error && <Alert variant="danger">{error}</Alert>}
                             <Form onSubmit={handleSubmit}>
+                                <Form.Group id="firstname">
+                                    <Form.Label>First Name</Form.Label>
+                                    <Form.Control type="text" placeholder="John" ref={emailRef} required></Form.Control>
+                                </Form.Group>
+                                <Form.Group id="lastname">
+                                    <Form.Label>Last Name</Form.Label>
+                                    <Form.Control type="text" placeholder="Doe" ref={emailRef} required></Form.Control>
+                                </Form.Group>
                                 <Form.Group id="email">
-                                    <Form.Label>Name</Form.Label>
-                                    <Form.Control type="email" placeholder="someone@email.com" ref={emailRef} required></Form.Control>  
-                                </Form.Group>
-                                <Form.Group id="password">
                                     <Form.Label>Email</Form.Label>
-                                    <Form.Control type="password" placeholder="Enter the password" ref={passwordRef} required></Form.Control>  
+                                    <Form.Control type="email" placeholder="someone@email.com" ref={emailRef} required></Form.Control>
                                 </Form.Group>
-                                <Button disabled={loading} className="w-100 btn" type="submit">Log In</Button>
+                                <Form.Group id="message">
+                                    <Form.Label>Message</Form.Label>
+                                    <Form.Control type="text" placeholder="Enter your message here" ref={emailRef} required></Form.Control>
+                                </Form.Group>                                
+                                <Button disabled={loading} className="w-100 btn" type="submit">Send</Button>
                             </Form>
                         </Card.Body>
                     </Card>
                     <div className="w-100 text-center mt-2">
-                        Don't have an account? <Link to="/Signup" className="sign-log-link">Sign Up</Link>
+                        Connect with us <Link to="/Signup" className="sign-log-link">Sign Up</Link>
                     </div>
                 </div>
             </Container>
--- front-end/src/Component/Home.js
+++ front-end/src/Component/Home.js
@@ -6,28 +6,28 @@ import Header from './Header.js';
 
 class Home extends React.Component {
 
-render(){
-    return (
-    <div>
-        <Header></Header>
-        <div className = "header">
-            <form>
-                <div className = "form-box">
-                    <input type = "text" className = "search-field" 
-                    placeholder = "Enter location"></input>
-                    <input type = "date" className = "search-field" 
-                    placeholder = "Check in date"></input>
-                    <input type = "date" className = "search-field" 
-                    placeholder = "Check out date"></input>
-                    <a class = "search-btn" href="#">
-                        <i class="fas fa-search-location"></i>
-                    </a>
+    render() {
+        return (
+            <div>
+                <Header></Header>
+                <div className="header">
+                    <form>
+                        <div className="form-box">
+                            <input type="text" className="search-field"
+                                placeholder="Enter location"></input>
+                            <input type="date" className="search-field"
+                                placeholder="Check in date"></input>
+                            <input type="date" className="search-field"
+                                placeholder="Check out date"></input>
+                            <a class="search-btn" href="#">
+                                <i class="fas fa-search-location"></i>
+                            </a>
+                        </div>
+                    </form>
+                    <h1 className="para">Park N'Go</h1>
+                    <button className="explore" type="button">Explore nearby locations</button>
                 </div>
-            </form>
-            <h1 className = "para">Park N'Go</h1>
-            <button className = "explore" type = "button">Explore nearby locations</button>
-        </div>
-            <div className="services">
+                <div className="services">
                 <div className="container">
                     <div className="firstrow row">
                         <div className="icon col-md-5">
@@ -35,7 +35,7 @@ render(){
                         </div>
                         <div className="searchPara col-md-7">
                             <h2>Search</h2>
-                        Ease your search for parkings in busy areas like donwtown by searching for parking-slots including residents' driveways
+                        Ease your search for parkings in busy areas like downtown by searching for parking-slots including residents' driveways
                         </div>
                     </div>
                     <div className="secondrow row">
@@ -44,7 +44,7 @@ render(){
                         Book for your parking spot at a nearby driveway at a convenient location
                         </div>
                         <div className="icon col-md-5">
-                            <img src="images/book1.png" alt="" />
+                            <img src="images/book.png" alt="" />
                         </div>
                     </div>
                     <div className="thirdrow row">
@@ -58,24 +58,19 @@ render(){
                     </div>
                 </div>
             </div>
-        <div className="cities">
-            <h3 className="cityTitle"> Featured Cities</h3>
-            <div className="allCities container">
-                <div className="row firstrow">
-                    <div className="imageTor col-md-3">Toronto</div>
-                    <div className="imageOta col-md-3">Ottawa</div>
-                    <div className="imageVan col-md-3">Vancover</div>
-                </div>    
-                <div className="row secondrow">
-                <div className="imageAlb col-md-3">Alberta</div>
-                <div className="imageMon col-md-3">Montreal</div>
-                <div className="imageQue col-md-3">Quebec</div>
+            
+               <div className="cities">
+                    <h3 className="city"> Featured Cities</h3>
+                    <div className="imageTor">Toronto</div>
+                    <div className="imageOta">Ottawa</div>
+                    <div className="imageVan">Vancover</div>
+                    <div className="imageAlb">Alberta</div>
+                    <div className="imageMon">Monteral</div>
+                    <div className="imageQue">Quebec</div>
                 </div>
+
             </div>
-        </div>
-        <Footer/>
-    </div>
-    );
+        );
     }
 }
 export default Home;
