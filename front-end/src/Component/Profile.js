2012d19951d335badc86af3dea71fd67db9f716c
hetpatel14 <hetpatel91314@gmail.com>
2020-11-30 19:40:00 -0500

    more on profile

--- front-end/src/Component/Profile.js
+++ front-end/src/Component/Profile.js
@@ -4,22 +4,15 @@ import Header from "./Header.js";
 import Footer from "./Footer.js";
 import firebase from '../firebase'
 
-
-
 export default function Profile() {
 
     const { currentUser, logout } = useAuth();
-
     const [locationsList, setlocationList] = useState();
 
     useEffect(() => {
-
         var str = currentUser.email.substr(0, currentUser.email.length - 4);
-
-        const locationRef=firebase.database().ref("Users/"+str);
-        
+        const locationRef = firebase.database().ref("Users/" + str);
         console.log(str);
-       
         locationRef.on("value", (snapshot) => {
             console.log(snapshot.val());
             const locations = snapshot.val();
@@ -32,31 +25,36 @@ export default function Profile() {
             setlocationList(locationsList);
         });
     }, [])
-    
 
-    return(
+    return (
         <div>
-           {/* <Header/> */}
-                <h1 className="greeting"> Hello, {currentUser.email}</h1>
-                {/* <div className="history">
-                <h1 className="historyHeading">
-                       Your history
+            <Header />
+            <h1 className="greeting"> Hello, {currentUser.email}</h1>
+            {locationsList ? locationsList.map((location) => (
+                <div className="history">
+                    <h1 className="historyHeading">
+                        Your history
                     </h1>
                     <div className="table" >
                         <table>
                             <tr>
+                                <th>Name</th>
+                                <th>Email</th>
                                 <th>Location</th>
-                                <th>Date</th>
-                            </tr>
+                                <th>Date of birth</th>
 
+                            </tr>
                             <tr>
-                                <td>401, Sunset Avenue</td>
-                                <td>1/12/2020</td>
+                                <td>{location.Name}</td>
+                                <td>{location.Email}</td>
+                                <td>{location.location}</td>
+                                <td>{location.DoB}</td>
                             </tr>
                         </table>
                     </div>
-                    </div>
-           <Footer /> */}
+                </div>
+            )) : "Loading"}
+            <Footer />
         </div>
     );
 
