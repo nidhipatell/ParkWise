2a775386ff44a1516287b59e9b961d802a0972d6
nandinipatel <patelnandini26@gmail.com>
2020-11-30 23:45:09 -0500

    final changes

--- front-end/src/Component/Profile.js
+++ front-end/src/Component/Profile.js
@@ -2,15 +2,23 @@ import { AuthProvider, useAuth } from "../contexts/AuthContext"
 import React, { Component, useRef, useState, useEffect } from 'react';
 import Header from "./Header.js";
 import Footer from "./Footer.js";
-import firebase from '../firebase'
+import firebase from '../firebase';
+import { useHistory } from "react-router-dom";
 
 export default function Profile() {
 
     const { currentUser, logout } = useAuth();
     const [locationsList, setlocationList] = useState();
 
+    const [dob, setdob] = useState();
+    const [name, setname] = useState();
+    const [email, setemail] = useState();
+    const [location, setlocation] = useState();
+    const history = useHistory();
+    
     useEffect(() => {
-        var str = currentUser.email.substr(0, currentUser.email.length - 4);
+        if (currentUser) {
+            var str = currentUser.email.substr(0, currentUser.email.length - 4);
         const locationRef = firebase.database().ref("Users/" + str);
         console.log(str);
         locationRef.on("value", (snapshot) => {
@@ -23,17 +31,24 @@ export default function Profile() {
             }
             console.log(locationsList);
             setlocationList(locationsList);
-        });
+            setdob(locationsList[0]);
+            setemail(locationsList[1]);
+            setlocation(locationsList[2]);
+            setname(locationsList[3]);
+          });}
+          else {
+            history.push("/");
+          }
     }, [])
 
     return (
         <div>
             <Header />
-            <h1 className="greeting"> Hello, {currentUser.email}</h1>
-            {locationsList ? locationsList.map((location) => (
+            <div className = "ov">
+            <h1 className="greeting"> Hello, {name}</h1>
                 <div className="history">
                     <h1 className="historyHeading">
-                        Your history
+                        Personal Information
                     </h1>
                     <div className="table" >
                         <table>
@@ -45,18 +60,33 @@ export default function Profile() {
 
                             </tr>
                             <tr>
-                                <td>{location.Name}</td>
-                                <td>{location.Email}</td>
-                                <td>{location.location}</td>
-                                <td>{location.DoB}</td>
+                                <td>{name}</td>
+                                <td>{email}</td>
+                                <td>{location}</td>
+                                <td>{dob}</td>
+                            </tr>
+                        </table>
+                        </div>
+                        <h1 className="historyHeading">
+                            Your history
+                        </h1>
+                        <div className = "table">
+                        <table>
+                            <tr>
+                                <th>Location</th>
+                                <th>Parking Date</th>
+                            </tr>
+                            <tr>
+                                <td>1150 St.Paul Avenue</td>
+                                <td>2/2/1970</td>
                             </tr>
                         </table>
                     </div>
                 </div>
-            )) : "Loading"}
+                </div>
+
             <Footer />
         </div>
     );
 
 }
-
